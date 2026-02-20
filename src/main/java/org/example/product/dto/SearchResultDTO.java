24ae18c3522de853c93b7bc55bd2721556fb8d21
Laukik <laukiknathjagbandhuvasyerp.com>
2026-02-20 15:40:26 +0530

    intial

---
+++ src/main/java/org/example/product/dto/SearchResultDTO.java
@@ -0,0 +1,13 @@
+package org.example.product.dto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SearchResultDTO {
+
+    @Query(value = """
+
+""")
+}
